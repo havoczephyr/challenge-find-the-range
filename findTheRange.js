228a4d61b820fb4518a36ff5d6baf174556e689d
Havoczephyr <havoczephyr@hotmail.com>
2021-10-12 22:01:08 -0400

    Revamped Variation based on using array.sort()

--- findTheRange.js
+++ findTheRange.js
@@ -1,16 +1,23 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, -36]
-let small = array [0]
-let big = array [0]
+// let small = array [0]
+// let big = array [0]
+// Write your solution below:
 
-for (let i = 0; i < array.length; i++) {
-  if (array [i] > big) {
-      big = array [i];
-  }
-  if (array [i] < small) {
-      small = array [i];
-  }
-}
+// for (let i = 0; i < array.length; i++) {
+//   if (array [i] > big) {
+//       big = array [i];
+//   }
+//   if (array [i] < small) {
+//       small = array [i];
+//   }
+// }
+// console.log(`Largest Number is: ${big}`);
+// console.log(`Smallest Number is: ${small}`);
+array.sort((a,b) => a-b)
+let small = array[0];
+let big = array[array.length-1];
+
 console.log(`Largest Number is: ${big}`);
 console.log(`Smallest Number is: ${small}`);
-// Write your solution below:
+
