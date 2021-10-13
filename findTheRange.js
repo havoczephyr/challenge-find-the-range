d62643368ba6d9f421645e815d4192fe37021498
Havoczephyr <havoczephyr@hotmail.com>
2021-10-12 21:09:02 -0400

    challenge complete :)

--- findTheRange.js
+++ findTheRange.js
@@ -1,4 +1,16 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, -36]
+let small = array [0]
+let big = array [0]
 
+for (let i = 0; i < array.length; i++) {
+  if (array [i] > big) {
+      big = array [i];
+  }
+  if (array [i] < small) {
+      small = array [i];
+  }
+}
+console.log(`Largest Number is: ${big}`);
+console.log(`Smallest Number is: ${small}`);
 // Write your solution below:
